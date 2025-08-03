368ed0692382074ed5a630fb4ffa8323b47b36ad
Ethan Davis <121524320+ethandav@users.noreply.github.com>
2025-08-02 17:22:44 -0700

    Addressing modes

--- src/AddressingModes.h
+++ src/AddressingModes.h
@@ -2,10 +2,17 @@
 #include "cpu.h"
 #include <cstdint>
 
-inline uint32_t absolute(CPU& cpu) {
+inline uint32_t absolute(CPU& cpu)
+{
     uint16_t lo = cpu.memory->read(cpu.registers.PC++);
     uint16_t hi = cpu.memory->read(cpu.registers.PC++);
     uint16_t addr = (hi << 8) | lo;
 
     return (cpu.registers.DBR << 16) | addr;
+}
+
+inline uint32_t absoluteX(CPU& cpu)
+{
+    uint32_t base_address = absolute(cpu);
+    return base_address + cpu.registers.X;
 }
--- src/opcodes.cpp
+++ src/opcodes.cpp
@@ -5,42 +5,59 @@
 
 void register_opcodes(CPU& cpu)
 {
+	// LDA Immediate
 	cpu.opcodeTable[0xA9] = [](CPU& cpu)
 		{
 			cpu.registers.A = cpu.memory->read(cpu.registers.PC++);
 		};
 
-	cpu.opcodeTable[0xA2] = [](CPU& cpu)
-		{
-			cpu.registers.X = cpu.memory->read(cpu.registers.PC++);
-		};
-
-	cpu.opcodeTable[0x00] = [](CPU& cpu)
-		{
-			cpu.running = false;
-		};
-
-	cpu.opcodeTable[0x8D] = [](CPU& cpu)
+	// LDA Absolute
+	cpu.opcodeTable[0xAD] = [](CPU& cpu)
 		{
 			uint32_t address = absolute(cpu);
-			cpu.memory->write(address, cpu.registers.A & 0xFF);
+			cpu.registers.A = cpu.memory->read(address);
 		};
 
-	cpu.opcodeTable[0x8E] = [](CPU& cpu)
+	// LDA Absolute Indexed,X
+	cpu.opcodeTable[0xBD] = [](CPU& cpu)
 		{
-			uint32_t address = absolute(cpu);
-			cpu.memory->write(address, cpu.registers.X & 0xFF);
+			uint32_t address = absoluteX(cpu);
+			cpu.registers.A = cpu.memory->read(address);
 		};
 
+	// LDA Absolute Indexed,Y
+
+	// LDX Absolute
 	cpu.opcodeTable[0xAE] = [](CPU& cpu)
 		{
 			uint32_t address = absolute(cpu);
 			cpu.registers.X = cpu.memory->read(address);
 		};
 
-	cpu.opcodeTable[0xAD] = [](CPU& cpu)
+	// LDX Immediate
+	cpu.opcodeTable[0xA2] = [](CPU& cpu)
+		{
+			cpu.registers.X = cpu.memory->read(cpu.registers.PC++);
+		};
+
+	// STA Absolute
+	cpu.opcodeTable[0x8D] = [](CPU& cpu)
 		{
 			uint32_t address = absolute(cpu);
-			cpu.registers.A = cpu.memory->read(address);
+			cpu.memory->write(address, cpu.registers.A & 0xFF);
 		};
+
+	// STX Absolute
+	cpu.opcodeTable[0x8E] = [](CPU& cpu)
+		{
+			uint32_t address = absolute(cpu);
+			cpu.memory->write(address, cpu.registers.X & 0xFF);
+		};
+
+	// BRK
+	cpu.opcodeTable[0x00] = [](CPU& cpu)
+		{
+			cpu.running = false;
+		};
+
 }
